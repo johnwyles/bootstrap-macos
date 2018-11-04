d63cb4635ccf9e786be5335ebc56cd43ca7ed049
John Wyles <john@johnwyles.com>
2018-11-04 10:39:54 -0800

    Fixing some bugs in the ruby installation of rbenv

--- scripts/programming/ruby.sh
+++ scripts/programming/ruby.sh
@@ -4,12 +4,14 @@
 brew install ruby-build
 brew install rbenv
 
-# From: https://github.com/joeyhoer/starter
+# Install rbenv and bundler (from: https://github.com/joeyhoer/starter)
 ruby_latest=$(rbenv install -l 2>/dev/null | awk '$1 ~ /^[0-9.]*$/ {latest=$1} END {print latest}')
+#curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 rbenv install $ruby_latest
 rbenv global $ruby_latest
+eval "$(rbenv init -)"
 gem install bundler
 rbenv rehash
 
- # Install the AWS gem
- gem install aws-sdk
+# Install the AWS gem
+gem install aws-sdk
